6791a8756206d43d79dde472ae047e72ad19697a
David Hull <davidhullster@gmail.com>
2021-03-13 15:18:42 -0800

    update test case for unittest

--- tests/test_text_tool.py
+++ tests/test_text_tool.py
@@ -2,10 +2,10 @@ from flask import Flask
 import unittest
 import json
 from project.text_tool.text_tool import app
+app.testing = True
+client = app.test_client()
 
 class TestTextTool(unittest.TestCase):
-    app.testing = True
-    client = app.test_client()
 
     def test_base_route(self):
         response = client.get('/')
