14ae2f7e14b60e56acbd2a0a631db00121d3caf1
David Hull <davidhullster@gmail.com>
2021-03-13 15:16:46 -0800

    update test case for unittest

--- tests/test_text_tool.py
+++ tests/test_text_tool.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import unittest
 import json
 from project.text_tool.text_tool import app
 
-app.testing = True
-client = app.test_client()
+class TestTextTool(unittest.TestCase):
+    app.testing = True
+    client = app.test_client()
 
-def test_base_route():
-    response = client.get('/')
-    assert response.status_code == 200
+    def test_base_route(self):
+        response = client.get('/')
+        assert response.status_code == 200
